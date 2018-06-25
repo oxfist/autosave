8a0fc6b3695fc0485f237ac6d733f254b487b206
Andrés Quilodrán <andres.caro.q@gmail.com>
2018-06-25 00:51:16 -0400

    Initial commit

---
+++ db/migrate/20180625044840_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.number :release_year
+      t.text :comment
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Game.delete_all
+
+Game.create!(
+  [
+    {
+      title: 'The Witcher 3: Wild Hunt',
+      release_year: 2015,
+      comment: 'I fucking love this game!'
+    }
+  ]
+)
